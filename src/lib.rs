ac7a5a0d613f09f5376235969fa66e8b93f9ca82
Folkert <folkert@folkertdev.nl>
2022-02-15 14:51:23 +0100

    implement the logic

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,11 @@
 #![cfg_attr(all(not(test), not(feature = "std")), no_std)]
 
-pub mod mock_flash;
+use embedded_storage::ReadStorage;
 
+pub mod mock_flash;
+pub mod tiny_mock_flash;
+
+#[derive(Debug)]
 pub struct NvmLog<F, T> {
     oldest_log_addr: u32,
     next_log_addr: u32,
@@ -31,27 +35,232 @@ impl<F: embedded_storage::nor_flash::NorFlash, T> NvmLog<F, T> {
         }
     }
 
-    pub fn erase_up_to_position(&mut self, position: &NvmLogPosition) -> Result<(), ()> {
-        todo!()
+    pub fn erase_up_to_position(&mut self, position: &NvmLogPosition) {
+        self.oldest_log_addr = position.next_log_addr;
+    }
+}
+
+const WORKING_BUF_SIZE: usize = 1000;
+
+#[derive(Debug)]
+enum CrossPageBoundary {
+    FitsInCurrentPage,
+    NextPage { start_address: usize },
+    BackToTheBeginning,
+}
+
+fn crosses_page_boundary<F>(flash: &F, start_address: usize, buf: &[u8]) -> CrossPageBoundary
+where
+    F: embedded_storage::nor_flash::NorFlash,
+{
+    let end_address = start_address + buf.len();
+
+    assert!(end_address - start_address <= F::ERASE_SIZE);
+
+    let starts_at_page_boundary = start_address % F::ERASE_SIZE == 0;
+    let crosses_page_boundary =
+        (start_address / F::ERASE_SIZE) != ((end_address - 1) / F::ERASE_SIZE);
+
+    if starts_at_page_boundary || crosses_page_boundary {
+        if end_address > flash.capacity() {
+            CrossPageBoundary::BackToTheBeginning
+        } else {
+            CrossPageBoundary::NextPage {
+                start_address: (end_address / F::ERASE_SIZE) * F::ERASE_SIZE,
+            }
+        }
+    } else {
+        CrossPageBoundary::FitsInCurrentPage
     }
 }
 
 impl<F: embedded_storage::nor_flash::NorFlash, T: serde::Serialize> NvmLog<F, T> {
-    fn store<const N: usize>(&mut self, value: T) -> Result<(), F::Error> {
-        todo!()
-    }
+    pub fn store(&mut self, value: T) -> Result<(), F::Error> {
+        let mut buf = [0u8; WORKING_BUF_SIZE];
+        let bytes = postcard::to_slice_cobs(&value, &mut buf).unwrap();
 
-    fn store_if_space<const N: usize>(&mut self, value: T) -> Result<(), F::Error> {
-        todo!()
+        use CrossPageBoundary::*;
+        match crosses_page_boundary(&self.flash, self.next_log_addr as usize, bytes) {
+            FitsInCurrentPage => {
+                // don't need to erase anything
+                self.flash.write(self.next_log_addr, bytes)?;
+                self.move_cursor_add(bytes.len() as u32)?;
+                Ok(())
+            }
+            NextPage { start_address } => {
+                // we will (partially) write into the next page; we must erase it first
+                self.flash
+                    .erase(start_address as u32, (start_address + F::ERASE_SIZE) as u32)?;
+
+                self.flash.write(self.next_log_addr, bytes)?;
+                self.move_cursor_add(bytes.len() as u32)?;
+                Ok(())
+            }
+            BackToTheBeginning => {
+                // NOTE the final bytes in the last page stay 0xFF
+                // we rely on this fact to see if there are messages
+                // in a given piece of memory (messages always end
+                // with a NULL byte when encoded with Cobs)
+                self.flash.erase(0, F::ERASE_SIZE as u32)?;
+
+                self.flash.write(0, bytes)?;
+                self.move_cursor_absolute(bytes.len() as u32)?;
+                Ok(())
+            }
+        }
     }
 }
 
-impl<'d, F: embedded_storage::nor_flash::NorFlash, T: serde::Deserialize<'d>> NvmLog<F, T> {
-    fn read<const N: usize>(&self, buf: &mut u8) -> Result<(), F::Error> {
-        todo!()
+impl<F: embedded_storage::nor_flash::ReadNorFlash, T> NvmLog<F, T> {
+    /// The next_log_addr is moved forward by `added`. It may now be bigger
+    /// than the `oldest_log_addr`, so it too needs to move forward to the
+    /// start of the next log
+    fn move_cursor_add(&mut self, added: u32) -> Result<(), F::Error> {
+        let new = self.next_log_addr + added;
+        if self.next_log_addr <= self.oldest_log_addr && new > self.oldest_log_addr {
+            self.move_cursor_absolute(new)
+        } else {
+            self.next_log_addr = new;
+            Ok(())
+        }
     }
 
-    fn read_and_erase<const N: usize>(&mut self, buf: &mut u8) -> Result<(), F::Error> {
-        todo!()
+    fn move_cursor_absolute(&mut self, new: u32) -> Result<(), F::Error> {
+        let mut next_oldest_log = 0;
+
+        if new >= self.oldest_log_addr {
+            for offset in new as usize..self.flash.capacity() {
+                let mut word = [0];
+                self.flash.read(offset as u32, &mut word)?;
+
+                // a NULL bytes is the COPS message sentinal (end) value
+                if word[0] == 0 {
+                    next_oldest_log = (offset + 1) % self.flash.capacity();
+                    break;
+                }
+            }
+
+            self.oldest_log_addr = next_oldest_log as u32;
+        }
+
+        self.next_log_addr = new;
+
+        Ok(())
+    }
+}
+
+pub struct NvmLogIter<'a, F, T> {
+    nvm_log: &'a mut NvmLog<F, T>,
+    span: Span,
+}
+
+impl<'a, F: embedded_storage::nor_flash::ReadNorFlash, T: serde::de::DeserializeOwned>
+    NvmLog<F, T>
+{
+    pub fn iter(&'a mut self) -> NvmLogIter<'a, F, T> {
+        let span = if self.oldest_log_addr < self.next_log_addr {
+            Span::Contiguous {
+                from: self.oldest_log_addr,
+                to: self.next_log_addr,
+            }
+        } else {
+            Span::BrokenUp {
+                from: self.oldest_log_addr,
+                to: self.next_log_addr,
+            }
+        };
+
+        NvmLogIter {
+            span,
+            nvm_log: self,
+        }
+    }
+}
+
+enum Span {
+    /// A contiguous chunk
+    Contiguous { from: u32, to: u32 },
+    /// The region wraps around somewhere
+    BrokenUp { from: u32, to: u32 },
+}
+
+impl Span {
+    fn is_empty(&self) -> bool {
+        if let Self::Contiguous { from, to } = self {
+            from == to
+        } else {
+            false
+        }
+    }
+
+    fn start(&self) -> u32 {
+        match self {
+            Self::Contiguous { from, .. } | Self::BrokenUp { from, .. } => *from,
+        }
+    }
+
+    fn step(&mut self, width: u32, capacity: u32) {
+        match self {
+            Self::Contiguous { from, .. } => {
+                *from += width;
+                assert!(*from <= capacity);
+            }
+            Self::BrokenUp { from, .. } => {
+                *from += width;
+                assert!(*from <= capacity);
+            }
+        }
+    }
+
+    fn wrap(&mut self) {
+        match &self {
+            Self::Contiguous { to, .. } => *self = Self::Contiguous { from: *to, to: *to },
+            Self::BrokenUp { to, .. } => *self = Self::Contiguous { from: 0, to: *to },
+        }
+    }
+}
+
+impl<'a, F: embedded_storage::nor_flash::ReadNorFlash, T: serde::de::DeserializeOwned> Iterator
+    for NvmLogIter<'a, F, T>
+{
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.span.is_empty() {
+            return None;
+        }
+
+        let remaining_bytes = self.nvm_log.flash.capacity() - self.span.start() as usize;
+        let remaining_bytes = remaining_bytes.min(WORKING_BUF_SIZE);
+
+        let mut buf = [0xFF; WORKING_BUF_SIZE];
+        let buf = &mut buf[..remaining_bytes];
+
+        match self.nvm_log.flash.read(self.span.start(), buf) {
+            Err(_e) => {
+                dbg!("flash read error");
+                None
+            }
+            Ok(()) => {
+                match buf.iter().position(|x| *x == 0) {
+                    None => {
+                        // there is no message here, we need to go look at the start
+                        self.span.wrap();
+                        self.next()
+                    }
+                    Some(message_end) => {
+                        let buf = &mut buf[..message_end];
+
+                        let capacity = self.nvm_log.flash.capacity() as u32;
+                        // +1 to skip the NULL byte
+                        self.span.step(message_end as u32 + 1, capacity);
+                        match postcard::from_bytes_cobs(buf) {
+                            Ok(e) => Some(e),
+                            Err(_e) => None,
+                        }
+                    }
+                }
+            }
+        }
     }
 }
---
+++ src/tiny_mock_flash.rs
@@ -0,0 +1,375 @@
+#![cfg(feature = "std")]
+
+use core::ops::Range;
+use embedded_storage::nor_flash::{NorFlash, ReadNorFlash};
+
+#[derive(Debug, Clone)]
+struct MockFlash {
+    writable: Vec<bool>,
+    words: Vec<u8>,
+}
+
+impl MockFlash {
+    const PAGES: usize = 3;
+    const BYTES_PER_WORD: usize = 1;
+
+    const CAPACITY_WORDS: usize = Self::PAGES * Self::PAGE_WORDS;
+    const CAPACITY_BYTES: usize = Self::CAPACITY_WORDS * Self::BYTES_PER_WORD;
+
+    const PAGE_WORDS: usize = 8;
+    const PAGE_BYTES: usize = Self::PAGE_WORDS * Self::BYTES_PER_WORD;
+
+    pub fn new() -> Self {
+        Self {
+            writable: vec![true; Self::CAPACITY_WORDS],
+            words: vec![u8::MAX; Self::CAPACITY_WORDS],
+        }
+    }
+
+    fn as_bytes(&self) -> &[u8] {
+        let ptr_words = self.words.as_ptr();
+        let ptr_bytes = ptr_words as *const u8;
+        unsafe { core::slice::from_raw_parts(ptr_bytes, Self::CAPACITY_BYTES) }
+    }
+
+    fn as_bytes_mut(&mut self) -> &mut [u8] {
+        let ptr_words = self.words.as_mut_ptr();
+        let ptr_bytes = ptr_words as *mut u8;
+        unsafe { core::slice::from_raw_parts_mut(ptr_bytes, Self::CAPACITY_BYTES) }
+    }
+
+    fn validate_read_operation(offset: u32, length: usize) -> Result<Range<usize>, MockFlashError> {
+        let offset = offset as usize;
+        if (offset % Self::BYTES_PER_WORD) != 0 {
+            Err(MockFlashError::Misaligned)
+        } else if offset > MockFlash::CAPACITY_BYTES || offset + length > MockFlash::CAPACITY_BYTES
+        {
+            Err(MockFlashError::OutOfBounds)
+        } else {
+            Ok(offset..(offset + length))
+        }
+    }
+
+    fn validate_write_operation(
+        &self,
+        offset: u32,
+        length: usize,
+    ) -> Result<Range<usize>, MockFlashError> {
+        let range = Self::validate_read_operation(offset, length)?;
+
+        let start_word = range.start / Self::BYTES_PER_WORD;
+        let end_word = (range.end + Self::BYTES_PER_WORD - 1) / Self::BYTES_PER_WORD;
+
+        let slice = &self.writable[start_word..end_word];
+        let it = (range.start..range.end).zip(slice.iter());
+        for (address, writable) in it {
+            if !writable {
+                return Err(MockFlashError::NotWritable(address as u32));
+            }
+        }
+
+        Ok(range)
+    }
+}
+
+#[derive(Debug)]
+enum MockFlashError {
+    OutOfBounds,
+    Misaligned,
+    NotWritable(u32),
+}
+
+impl ReadNorFlash for MockFlash {
+    type Error = MockFlashError;
+
+    const READ_SIZE: usize = Self::BYTES_PER_WORD;
+
+    fn read(&mut self, offset: u32, bytes: &mut [u8]) -> Result<(), Self::Error> {
+        let range = Self::validate_read_operation(offset, bytes.len())?;
+
+        bytes.copy_from_slice(&self.as_bytes()[range]);
+
+        Ok(())
+    }
+
+    fn capacity(&self) -> usize {
+        Self::CAPACITY_BYTES
+    }
+}
+
+impl NorFlash for MockFlash {
+    const WRITE_SIZE: usize = Self::BYTES_PER_WORD;
+
+    const ERASE_SIZE: usize = Self::PAGE_BYTES;
+
+    fn erase(&mut self, from: u32, to: u32) -> Result<(), Self::Error> {
+        let from = from as usize;
+        let to = to as usize;
+
+        assert!(from <= to);
+
+        if to > Self::CAPACITY_BYTES {
+            return Err(MockFlashError::OutOfBounds);
+        }
+
+        if from % Self::PAGE_BYTES != 0 || to % Self::PAGE_BYTES != 0 {
+            return Err(MockFlashError::Misaligned);
+        }
+
+        for byte in self.as_bytes_mut()[from..to].iter_mut() {
+            *byte = u8::MAX;
+        }
+
+        let range = from / Self::BYTES_PER_WORD..to / Self::BYTES_PER_WORD;
+        for word_writable in self.writable[range].iter_mut() {
+            *word_writable = true;
+        }
+
+        Ok(())
+    }
+
+    fn write(&mut self, offset: u32, bytes: &[u8]) -> Result<(), Self::Error> {
+        println!("Writing at offset {} {:?}", offset, bytes);
+        let range = self.validate_write_operation(offset, bytes.len())?;
+
+        let start_word = range.start / Self::BYTES_PER_WORD;
+        let end_word = (range.end + Self::BYTES_PER_WORD - 1) / Self::BYTES_PER_WORD;
+
+        self.as_bytes_mut()[range].copy_from_slice(bytes);
+
+        for word_writable in self.writable[start_word..end_word].iter_mut() {
+            *word_writable = false;
+        }
+
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::NvmLog;
+    // use std::assert_matches::assert_matches;
+
+    #[test]
+    fn double_write_gives_error() {
+        let mut flash = MockFlash::new();
+
+        flash.write(0, &[0xAB]).unwrap();
+
+        assert!(matches!(
+            flash.write(0, &[0xCD]),
+            Err(MockFlashError::NotWritable(0))
+        ))
+    }
+
+    #[test]
+    fn out_of_bounds_read_gives_error() {
+        let mut flash = MockFlash::new();
+
+        let mut output = [0; 1];
+
+        assert!(matches!(
+            flash.read(MockFlash::CAPACITY_BYTES as u32 + 4, &mut output),
+            Err(MockFlashError::OutOfBounds)
+        ))
+    }
+
+    #[test]
+    fn out_of_bounds_write_gives_error() {
+        let mut flash = MockFlash::new();
+
+        assert!(matches!(
+            flash.write(MockFlash::CAPACITY_BYTES as u32 + 4, &[0xCD]),
+            Err(MockFlashError::OutOfBounds)
+        ))
+    }
+
+    #[test]
+    fn write_two_words() {
+        let mut nvm_log: NvmLog<MockFlash, u8> = NvmLog::new(MockFlash::new());
+
+        for i in 0..2 {
+            nvm_log.store(i as u8).unwrap();
+        }
+
+        assert_eq!(
+            &nvm_log.flash.words,
+            &[
+                1, 1, 0, 2, 1, 0, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+                255, 255, 255, 255, 255,
+            ]
+        );
+
+        assert_eq!(
+            &nvm_log.flash.writable,
+            &[
+                false, false, false, false, false, false, true, true, true, true, true, true, true,
+                true, true, true, true, true, true, true, true, true, true, true,
+            ]
+        );
+    }
+
+    #[test]
+    fn write_two_words_erase_page() {
+        let mut nvm_log: NvmLog<MockFlash, u8> = NvmLog::new(MockFlash::new());
+
+        for i in 0..2 {
+            nvm_log.store(i as u8).unwrap();
+        }
+
+        nvm_log.flash.erase(8, 16).unwrap();
+
+        assert_eq!(
+            &nvm_log.flash.words,
+            &[
+                1, 1, 0, 2, 1, 0, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+                255, 255, 255, 255, 255,
+            ]
+        );
+
+        assert_eq!(
+            &nvm_log.flash.writable,
+            &[
+                false, false, false, false, false, false, true, true, true, true, true, true, true,
+                true, true, true, true, true, true, true, true, true, true, true,
+            ]
+        );
+    }
+
+    #[test]
+    fn write_three_words() {
+        let mut nvm_log: NvmLog<MockFlash, u8> = NvmLog::new(MockFlash::new());
+
+        // this will cross a page boundary
+        for i in 0..3 {
+            nvm_log.store(i as u8).unwrap();
+        }
+
+        assert_eq!(
+            &nvm_log.flash.words,
+            &[
+                1, 1, 0, 2, 1, 0, 2, 2, 0, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+                255, 255, 255, 255,
+            ]
+        );
+
+        assert_eq!(
+            &nvm_log.flash.writable,
+            &[
+                false, false, false, false, false, false, false, false, false, true, true, true,
+                true, true, true, true, true, true, true, true, true, true, true, true,
+            ]
+        );
+    }
+
+    #[test]
+    fn write_eight_words() {
+        let mut nvm_log: NvmLog<MockFlash, u8> = NvmLog::new(MockFlash::new());
+
+        // this will fill up all memory, but not wrap around yet
+        for i in 0..8 {
+            nvm_log.store(i as u8).unwrap();
+        }
+
+        assert_eq!(
+            &nvm_log.flash.words,
+            &[1, 1, 0, 2, 1, 0, 2, 2, 0, 2, 3, 0, 2, 4, 0, 2, 5, 0, 2, 6, 0, 2, 7, 0,]
+        );
+
+        assert_eq!(
+            &nvm_log.flash.writable,
+            &[
+                false, false, false, false, false, false, false, false, false, false, false, false,
+                false, false, false, false, false, false, false, false, false, false, false, false,
+            ]
+        );
+    }
+
+    #[test]
+    fn write_nine_words() {
+        let mut nvm_log: NvmLog<MockFlash, u8> = NvmLog::new(MockFlash::new());
+
+        // this will wrap around
+        for i in 0..9 {
+            nvm_log.store(i as u8).unwrap();
+            dbg!(&nvm_log.flash);
+        }
+
+        assert_eq!(
+            &nvm_log.flash.words,
+            &[2, 8, 0, 255, 255, 255, 255, 255, 0, 2, 3, 0, 2, 4, 0, 2, 5, 0, 2, 6, 0, 2, 7, 0]
+        );
+
+        assert_eq!(
+            &nvm_log.flash.writable,
+            &[
+                false, false, false, true, true, true, true, true, false, false, false, false,
+                false, false, false, false, false, false, false, false, false, false, false, false,
+            ]
+        );
+
+        // next-oldest message is the first message on the second page
+        assert_eq!(nvm_log.oldest_log_addr, 9);
+    }
+
+    #[test]
+    fn read_middle_to_back() {
+        let mut nvm_log: NvmLog<MockFlash, u8> = NvmLog::new(MockFlash::new());
+
+        for i in 0..3 {
+            nvm_log.store(i as u8).unwrap();
+        }
+
+        let it = nvm_log.iter();
+
+        let messages: Vec<_> = it.collect();
+
+        assert_eq!(vec![0, 1, 2], messages);
+    }
+
+    #[test]
+    fn read_full() {
+        let mut nvm_log: NvmLog<MockFlash, u8> = NvmLog::new(MockFlash::new());
+
+        for i in 0..8 {
+            nvm_log.store(i as u8).unwrap();
+        }
+
+        let it = nvm_log.iter();
+
+        let messages: Vec<_> = it.collect();
+
+        assert_eq!(vec![0, 1, 2, 3, 4, 5, 6, 7], messages);
+    }
+
+    #[test]
+    fn read_disjoint() {
+        let mut nvm_log: NvmLog<MockFlash, u8> = NvmLog::new(MockFlash::new());
+
+        for i in 0..9 {
+            nvm_log.store(i as u8).unwrap();
+        }
+
+        let it = nvm_log.iter();
+
+        let messages: Vec<_> = it.collect();
+
+        assert_eq!(vec![3, 4, 5, 6, 7, 8], messages);
+    }
+
+    #[test]
+    fn foobar() {
+        let mut nvm_log: NvmLog<MockFlash, u8> = NvmLog::new(MockFlash::new());
+
+        for i in 0..9 {
+            nvm_log.store(i as u8).unwrap();
+        }
+
+        let it = nvm_log.iter();
+
+        let messages: Vec<_> = it.collect();
+
+        dbg!(messages);
+    }
+}
