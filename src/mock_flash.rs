386f5b1bc41d4c763dcb013e92477c77eb25ed10
Folkert <folkert@folkertdev.nl>
2022-03-06 22:39:52 +0100

    brining mock_flash up to date

--- src/mock_flash.rs
+++ src/mock_flash.rs
@@ -2,11 +2,23 @@
 
 use core::ops::Range;
 use embedded_storage::nor_flash::{
-    ErrorType, NorFlash, NorFlashError, NorFlashErrorKind, ReadNorFlash,
+    ErrorType, MultiwriteNorFlash, NorFlash, NorFlashError, NorFlashErrorKind, ReadNorFlash,
 };
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum Writable {
+    /// Twice
+    T,
+    /// Once (can only convert 1 bits to 0
+    O,
+    /// Never (must be cleared before being writable again)
+    N,
+}
+
+use Writable::*;
+
 pub struct MockFlash {
-    writable: Vec<bool>,
+    writable: Vec<Writable>,
     words: Vec<u32>,
 }
 
@@ -23,12 +35,12 @@ impl MockFlash {
     const CAPACITY_WORDS: usize = Self::PAGES * Self::PAGE_WORDS;
     const CAPACITY_BYTES: usize = Self::CAPACITY_WORDS * Self::BYTES_PER_WORD;
 
-    const PAGE_WORDS: usize = 1024;
+    const PAGE_WORDS: usize = 32; // 1024;
     const PAGE_BYTES: usize = Self::PAGE_WORDS * Self::BYTES_PER_WORD;
 
     pub fn new() -> Self {
         Self {
-            writable: vec![true; Self::CAPACITY_WORDS],
+            writable: vec![T; Self::CAPACITY_WORDS],
             words: vec![u32::MAX; Self::CAPACITY_WORDS],
         }
     }
@@ -70,7 +82,7 @@ impl MockFlash {
         let slice = &self.writable[start_word..end_word];
         let it = (range.start..range.end).zip(slice.iter());
         for (address, writable) in it {
-            if !writable {
+            if *writable == Writable::N {
                 return Err(MockFlashError::NotWritable(address as u32));
             }
         }
@@ -101,7 +113,7 @@ impl ErrorType for MockFlash {
 }
 
 impl ReadNorFlash for MockFlash {
-    const READ_SIZE: usize = Self::BYTES_PER_WORD;
+    const READ_SIZE: usize = 1;
 
     fn read(&mut self, offset: u32, bytes: &mut [u8]) -> Result<(), Self::Error> {
         let range = Self::validate_read_operation(offset, bytes.len())?;
@@ -115,6 +127,8 @@ impl ReadNorFlash for MockFlash {
         Self::CAPACITY_BYTES
     }
 }
+
+impl MultiwriteNorFlash for MockFlash {}
 
 impl NorFlash for MockFlash {
     const WRITE_SIZE: usize = Self::BYTES_PER_WORD;
@@ -141,7 +155,7 @@ impl NorFlash for MockFlash {
 
         let range = from / Self::BYTES_PER_WORD..to / Self::BYTES_PER_WORD;
         for word_writable in self.writable[range].iter_mut() {
-            *word_writable = true;
+            *word_writable = T;
         }
 
         Ok(())
@@ -150,13 +164,23 @@ impl NorFlash for MockFlash {
     fn write(&mut self, offset: u32, bytes: &[u8]) -> Result<(), Self::Error> {
         let range = self.validate_write_operation(offset, bytes.len())?;
 
+        if bytes.len() % Self::WRITE_SIZE != 0 {
+            panic!("any write must be a multiple of Self::WRITE_SIZE bytes");
+        }
+
         let start_word = range.start / Self::BYTES_PER_WORD;
         let end_word = (range.end + Self::BYTES_PER_WORD - 1) / Self::BYTES_PER_WORD;
 
-        self.as_bytes_mut()[range].copy_from_slice(bytes);
+        for (target, source) in self.as_bytes_mut()[range].iter_mut().zip(bytes.iter()) {
+            *target &= *source;
+        }
 
         for word_writable in self.writable[start_word..end_word].iter_mut() {
-            *word_writable = false;
+            *word_writable = match *word_writable {
+                Writable::T => Writable::O,
+                Writable::O => Writable::N,
+                Writable::N => Writable::N,
+            };
         }
 
         Ok(())
@@ -408,5 +432,152 @@ mod test {
         // assert_eq!(expected, messages);
 
         dbg!(&messages);
+    }
+
+    #[test]
+    fn iterate_erase_iterate() {
+        use LogMessage::*;
+        use TickToUnixResult::*;
+
+        let flash = MockFlash::new();
+        let mut nvm_log: NvmLog<MockFlash, LogEntry> = NvmLog::new_infer_position(flash);
+
+        let messages: Vec<LogEntry> = vec![
+            LogEntry {
+                msg: DeviceBoot,
+                timestamp: NotSynchronized,
+            },
+            LogEntry {
+                msg: ResetCode(ArrayString::from("Watchdog").unwrap()),
+                timestamp: NotSynchronized,
+            },
+            LogEntry {
+                msg: MovementReported,
+                timestamp: Success(DateTime(1646056005532)),
+            },
+        ];
+
+        for msg in messages.clone() {
+            nvm_log.store(msg).unwrap();
+        }
+
+        let start_position = nvm_log.current_position();
+        let mut it = nvm_log.result_iter();
+        let first: Vec<_> = (&mut it).flatten().collect();
+
+        let mut nvm_log = it.free();
+
+        let slice = &nvm_log.flash.words[..8];
+        let slice2 =
+            unsafe { core::slice::from_raw_parts(slice.as_ptr() as *const u8, slice.len() * 4) };
+
+        dbg!(slice2);
+
+        nvm_log.erase_up_to_position(&start_position).unwrap();
+
+        let slice = &nvm_log.flash.words[..8];
+        let slice2 =
+            unsafe { core::slice::from_raw_parts(slice.as_ptr() as *const u8, slice.len() * 4) };
+
+        dbg!(slice2);
+
+        let it = nvm_log.result_iter();
+        let second: Vec<_> = it.flatten().collect();
+
+        assert_eq!(first, messages);
+        assert_eq!(second, vec![]);
+    }
+
+    #[test]
+    fn find_the_start_after_erase() {
+        use LogMessage::*;
+        use TickToUnixResult::*;
+
+        const MEMORY: [u8; 256] = [
+            128, 3, 2, 1, 0, 0, 0, 0, 128, 15, 3, 11, 83, 121, 115, 82, 101, 115, 101, 116, 82,
+            101, 113, 1, 0, 0, 0, 0, 128, 3, 2, 1, 0, 0, 0, 0, 128, 17, 3, 13, 87, 97, 107, 101,
+            117, 112, 70, 114, 111, 109, 79, 102, 102, 1, 0, 0, 128, 2, 6, 7, 250, 254, 196, 69,
+            127, 1, 1, 1, 0, 0, 0, 0, 128, 3, 2, 1, 0, 0, 0, 0, 128, 12, 3, 8, 82, 101, 115, 101,
+            116, 80, 105, 110, 1, 0, 0, 0, 128, 2, 6, 7, 31, 244, 197, 69, 127, 1, 1, 1, 0, 0, 0,
+            0, 192, 3, 2, 1, 0, 0, 0, 0, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+            255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+            255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+            255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+            255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+            255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+            255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+            255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+            255, 255, 255, 255, 255, 255,
+        ];
+
+        let memory_u32: [u32; MEMORY.len() / 4] = unsafe { core::mem::transmute(MEMORY) };
+
+        let mut flash = MockFlash::new();
+        flash.words[..MEMORY.len() / 4].copy_from_slice(&memory_u32);
+
+        let nvm_log: NvmLog<MockFlash, LogEntry> = NvmLog::new_infer_position(flash);
+
+        let messages: Vec<_> = nvm_log.result_iter().flatten().collect();
+
+        let expected = vec![LogEntry {
+            msg: DeviceBoot,
+            timestamp: NotSynchronized,
+        }];
+
+        assert_eq!(expected, messages);
+    }
+
+    #[test]
+    fn fill_it_up() {
+        use LogMessage::*;
+        use TickToUnixResult::*;
+
+        let flash = MockFlash::new();
+        let mut nvm_log: NvmLog<MockFlash, LogEntry> = NvmLog::new_infer_position(flash);
+
+        let messages: Vec<LogEntry> = vec![
+            LogEntry {
+                msg: DeviceBoot,
+                timestamp: NotSynchronized,
+            },
+            LogEntry {
+                msg: ResetCode(ArrayString::from("Watchdog").unwrap()),
+                timestamp: NotSynchronized,
+            },
+            LogEntry {
+                msg: MovementReported,
+                timestamp: Success(DateTime(1646056005532)),
+            },
+        ];
+
+        for msg in messages.iter().cycle().take(10) {
+            nvm_log.store(msg.clone()).unwrap();
+        }
+
+        // dbg!(&nvm_log.flash.as_bytes());
+
+        let messages: Vec<_> = nvm_log.result_iter().collect();
+
+        dbg!(messages.len());
+
+        // dbg!(messages.len());
+
+        /*
+        let position = nvm_log.current_position();
+
+        // this will wrap around
+        for msg in messages.into_iter().cycle().take(5) {
+            nvm_log.store(msg).unwrap();
+        }
+
+        dbg!(&nvm_log.flash.as_bytes()[120..140]);
+
+        nvm_log.erase_up_to_position(&position).unwrap();
+
+        dbg!(&nvm_log.flash.as_bytes());
+        let messages: Vec<_> = nvm_log.iter().collect();
+
+        dbg!(messages.len(), messages);
+        */
     }
 }
